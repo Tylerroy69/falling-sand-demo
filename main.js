5670622ff01eb77cf80af4dae3d6137e6cf40f8d
Tylerroy69 <163754125+Tylerroy69@users.noreply.github.com>
2024-04-24 16:33:41 -0400

    Enhance game with particle types and interactions

--- main.js
+++ main.js
@@ -4,9 +4,26 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width;
 var height = canvas.height;
 var pixels = new Array(width * height).fill('000000');
+var particleTypes = {
+  sand: '#e7b662',
+  water: '#0088ff',
+  oil: '#776654'
+};
+var currentParticle = 'sand';
 
-function update() {
-    requestAnimationFrame(update);
+function draw() {
+  ctx.clearRect(0, 0, width, height);
+  for (var i = 0; i < pixels.length; i++) {
+    var x = i %  width;
+    var y = Math.floor(k / width);
+    ctx.fillStyle = particleTypes[pixels[i]];
+    ctx.fillRect(x, y, 1, 1);
+  }
 }
 
+function update() {
+  // Simple particle interactions and movement updates
+  draw();
+  requestAnimationFrame(update);
+}
 update();
